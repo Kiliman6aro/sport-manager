c03f8c2f806414d28d112dedff22f296029fe811
B.Pavel <kiliman6aro@gmail.com>
2025-02-28 10:23:30 +0200

    Remove not used packages

--- src/main/java/ua/pp/hophey/pushupapp/workoutlib/model/Exercise.java
+++ src/main/java/ua/pp/hophey/pushupapp/workoutlib/model/Exercise.java
@@ -3,7 +3,6 @@ package ua.pp.hophey.pushupapp.workoutlib.model;
 import ua.pp.hophey.pushupapp.workoutlib.event.EventBus;
 import ua.pp.hophey.pushupapp.workoutlib.event.ExerciseFinishedEvent;
 import ua.pp.hophey.pushupapp.workoutlib.event.ExerciseStartedEvent;
-import ua.pp.hophey.pushupapp.workoutlib.event.WorkoutEvent;
 
 public class Exercise implements Executable {
 
@@ -15,15 +14,11 @@ public class Exercise implements Executable {
 
     @Override
     public void start() {
-//        System.out.println("Упражнение началось");
-//        EventBus.getInstance().post("Упражнение началось");
         EventBus.getInstance().post(new ExerciseStartedEvent(this, durationMillis));
     }
 
     @Override
     public void stop() {
-//        System.out.println("Упражнение коничлось");
-//        EventBus.getInstance().post("Упражнение коничлось");
         EventBus.getInstance().post(new ExerciseFinishedEvent(this));
     }
 
